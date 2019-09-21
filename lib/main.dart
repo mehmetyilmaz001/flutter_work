810b2c0a19c5c7cbcac15275772429c5210d98fe
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2019-09-21 17:02:42 +0300

    State

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,58 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 
-class MyApp extends StatelessWidget{
-  
-@override
- Widget build(BuildContext context) {
-   return (
-     MaterialApp(
-       home: Text('Hello World'),
-     )
-   );
- }
 
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp>{
+  var _questionIndex = 0;
+  
+  void onAnswerQuestion() 
+  {
+    setState(() {
+      _questionIndex = _questionIndex + 1;  
+    });
+    
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  
+    var questions = [
+      'What\'s your favourite color?',
+      'Whats\'s your favourite animal?',
+    ];
+
+    return (MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Workout App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions.elementAt(_questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => onAnswerQuestion(),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => onAnswerQuestion(),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => onAnswerQuestion(),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
 }
